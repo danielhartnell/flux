7b16e540df0ed63a5a16991597b8ed48c09462e5
Roland Schilter <roli@weave.works>
2018-07-12 18:51:35 -0700

    Add `flux release --interactive`
    This adds an `--interactive` option to the `release` command that lets
    you pick and choose which containers to update. It uses the new
    `containers` specification in the /v9/update-manifest call.
    
    Once the containers are selected, the release is sent off with
    the constraint that the state of the containers need to be the exact
    same as when the results were presented to the user. If anything changed
    in the meanwhile, the whole release will not be processed.

---
+++ update/menu.go
@@ -0,0 +1,323 @@
+package update
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+
+	"github.com/pkg/term"
+	"golang.org/x/sys/unix"
+
+	"github.com/weaveworks/flux"
+)
+
+// Escape sequences.
+const moveCursorUp = "\033[%dA"
+const moveStartOfLine = "\r"
+const hideCursor = "\033[?25l"
+const showCursor = "\033[?25h"
+
+type WriteFlusher interface {
+	io.Writer
+	Flush() error
+}
+
+type ClearableLineWriter struct {
+	wf    WriteFlusher
+	lines int    // line count
+	width uint16 // terminal width
+}
+
+func NewClearableWriter(wf WriteFlusher) *ClearableLineWriter {
+	ws, _ := unix.IoctlGetWinsize(int(os.Stdout.Fd()), unix.TIOCGWINSZ)
+	w := &ClearableLineWriter{wf: wf, lines: 0, width: 9999}
+	if ws != nil && ws.Col != 0 {
+		w.width = ws.Col
+	}
+	return w
+}
+
+// Writeln counts the lines we output.
+func (c *ClearableLineWriter) Writeln(line string) error {
+	line += "\n"
+	c.lines += (len(line)-1)/int(c.width) + 1
+	_, err := c.wf.Write([]byte(line))
+	return err
+}
+
+// Clear moves the terminal cursor up to the beginning of the
+// line where we started writing.
+func (c *ClearableLineWriter) Clear() {
+	if c.lines == 0 {
+		return
+	}
+	fmt.Fprintf(c.wf, moveCursorUp, c.lines)
+	fmt.Fprintf(c.wf, moveStartOfLine)
+	c.lines = 0
+}
+
+func (c *ClearableLineWriter) Flush() error {
+	return c.wf.Flush()
+}
+
+type menuItem struct {
+	id     flux.ResourceID
+	status ControllerUpdateStatus
+	error  string
+	update ContainerUpdate
+
+	selected bool
+}
+
+type Menu struct {
+	out        *ClearableLineWriter
+	items      []menuItem
+	selectable int
+	cursor     int
+}
+
+// NewMenu creates a menu printer that outputs a result set to
+// the `io.Writer` provided, at the given level of verbosity:
+//  - 2 = include skipped and ignored resources
+//  - 1 = include skipped resources, exclude ignored resources
+//  - 0 = exclude skipped and ignored resources
+//
+// It can print a one time listing with `Print()` or then enter
+// interactive mode with `Run()`.
+func NewMenu(out io.Writer, results Result, verbosity int) *Menu {
+	m := &Menu{
+		out: NewClearableWriter(tabwriter.NewWriter(out, 0, 2, 2, ' ', 0)),
+	}
+	m.fromResults(results, verbosity)
+	return m
+}
+
+func (m *Menu) fromResults(results Result, verbosity int) {
+	for _, serviceID := range results.ServiceIDs() {
+		resourceID := flux.MustParseResourceID(serviceID)
+		result := results[resourceID]
+		switch result.Status {
+		case ReleaseStatusIgnored:
+			if verbosity < 2 {
+				continue
+			}
+		case ReleaseStatusSkipped:
+			if verbosity < 1 {
+				continue
+			}
+		}
+
+		if result.Error != "" {
+			m.AddItem(menuItem{
+				id:     resourceID,
+				status: result.Status,
+				error:  result.Error,
+			})
+		}
+		for _, upd := range result.PerContainer {
+			m.AddItem(menuItem{
+				id:     resourceID,
+				status: result.Status,
+				update: upd,
+			})
+		}
+		if result.Error == "" && len(result.PerContainer) == 0 {
+			m.AddItem(menuItem{
+				id:     resourceID,
+				status: result.Status,
+			})
+		}
+	}
+	return
+}
+
+func (m *Menu) AddItem(mi menuItem) {
+	if mi.selectable() {
+		mi.selected = true
+		m.selectable++
+	}
+	m.items = append(m.items, mi)
+}
+
+// Run starts the interactive menu mode.
+func (m *Menu) Run() (specs map[flux.ResourceID][]ContainerUpdate, aborted bool) {
+	specs = make(map[flux.ResourceID][]ContainerUpdate)
+
+	m.doPrint(true)
+	fmt.Printf(hideCursor)
+	defer fmt.Printf(showCursor)
+
+	for {
+		ascii, keyCode, err := getChar()
+		if err != nil {
+			return specs, true
+		}
+
+		switch ascii {
+		case 3, 27, 'q':
+			return specs, true
+		case ' ':
+			m.toggleCursor()
+		case 13:
+			for _, item := range m.items {
+				if item.selected {
+					specs[item.id] = append(specs[item.id], item.update)
+				}
+			}
+			m.out.Writeln("")
+			return
+		case 9, 'j':
+			m.cursorDown()
+		case 'k':
+			m.cursorUp()
+		}
+
+		switch keyCode {
+		case 40:
+			m.cursorDown()
+		case 38:
+			m.cursorUp()
+		}
+	}
+	return
+}
+
+func (m *Menu) Print() {
+	m.doPrint(false)
+}
+
+func (m *Menu) doPrint(interactive bool) {
+	if interactive {
+		m.out.Clear()
+	}
+	heading := "CONTROLLER \tSTATUS \tUPDATES"
+	if interactive {
+		heading = "   " + heading
+	}
+
+	m.out.Writeln(heading)
+	i := 0
+	var previd flux.ResourceID
+	for _, item := range m.items {
+		inline := previd == item.id
+		if interactive {
+			m.out.Writeln(m.renderInteractiveItem(item, inline, i))
+		} else {
+			m.out.Writeln(m.renderItem(item, inline))
+		}
+
+		previd = item.id
+		if item.selectable() {
+			i++
+		}
+	}
+	if interactive {
+		m.out.Writeln("")
+		m.out.Writeln("Use [Space] to deselect containers and hit [Enter] to release selected.")
+	}
+
+	m.out.Flush()
+}
+
+func (m *Menu) renderItem(item menuItem, inline bool) string {
+	if inline {
+		return fmt.Sprintf("\t\t%s", item.updates())
+	} else {
+		return fmt.Sprintf("%s\t%s\t%s", item.id, item.status, item.updates())
+	}
+}
+
+func (m *Menu) renderInteractiveItem(item menuItem, inline bool, index int) string {
+	pre := bytes.Buffer{}
+	if index == m.cursor {
+		pre.WriteString(">")
+	} else {
+		pre.WriteString(" ")
+	}
+	pre.WriteString(item.checkbox())
+	pre.WriteString(" ")
+	pre.WriteString(m.renderItem(item, inline))
+
+	return pre.String()
+}
+
+func (m *Menu) toggleCursor() {
+	m.items[m.cursor].selected = !m.items[m.cursor].selected
+	m.doPrint(true)
+}
+
+func (m *Menu) cursorDown() {
+	m.cursor = (m.cursor + 1) % m.selectable
+	m.doPrint(true)
+}
+
+func (m *Menu) cursorUp() {
+	m.cursor = (m.cursor + m.selectable - 1) % m.selectable
+	m.doPrint(true)
+}
+
+func (i menuItem) checkbox() string {
+	switch {
+	case !i.selectable():
+		return " "
+	case i.selected:
+		return "\u25c9"
+	default:
+		return "\u25ef"
+	}
+}
+
+func (i menuItem) selectable() bool {
+	return i.update.Container != ""
+}
+
+func (i menuItem) updates() string {
+	if i.update.Container != "" {
+		return fmt.Sprintf("%s: %s -> %s",
+			i.update.Container,
+			i.update.Current.String(),
+			i.update.Target.Tag)
+	}
+	return i.error
+}
+
+// See https://github.com/paulrademacher/climenu/blob/master/getchar.go
+func getChar() (ascii int, keyCode int, err error) {
+	t, _ := term.Open("/dev/tty")
+	term.RawMode(t)
+	bs := make([]byte, 3)
+
+	var numRead int
+	numRead, err = t.Read(bs)
+	if err != nil {
+		return
+	}
+	if numRead == 3 && bs[0] == 27 && bs[1] == 91 {
+		// Three-character control sequence, beginning with "ESC-[".
+
+		// Since there are no ASCII codes for arrow keys, we use
+		// Javascript key codes.
+		if bs[2] == 65 {
+			// Up
+			keyCode = 38
+		} else if bs[2] == 66 {
+			// Down
+			keyCode = 40
+		} else if bs[2] == 67 {
+			// Right
+			keyCode = 39
+		} else if bs[2] == 68 {
+			// Left
+			keyCode = 37
+		}
+	} else if numRead == 1 {
+		ascii = int(bs[0])
+	} else {
+		// Two characters read??
+	}
+	t.Restore()
+	t.Close()
+	return
+}
